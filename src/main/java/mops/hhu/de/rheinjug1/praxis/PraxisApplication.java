4d2c477a3db3b551efeb564d810cbd0dd1031d5e
simon-perschel@t-online.de <bywguc-1mabtY-wigwaz>
2020-03-03 09:36:57 +0100

    chore: initial commit

---
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/PraxisApplication.java
@@ -0,0 +1,13 @@
+package mops.hhu.de.rheinjug1.praxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraxisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraxisApplication.class, args);
+    }
+
+}
