1b32ae3ec6114dde2fb5a8c85392d7a580f3e98c
simon-perschel@t-online.de <bywguc-1mabtY-wigwaz>
2020-03-03 09:36:57 +0100

    chore: initial commit

---
+++ src/test/java/mops/hhu/de/rheinjug1/praxis/PraxisApplicationTests.java
@@ -0,0 +1,13 @@
+package mops.hhu.de.rheinjug1.praxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraxisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
