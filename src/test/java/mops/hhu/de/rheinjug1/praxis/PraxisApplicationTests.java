a5310f72b0abd6f1f7a9edde6e3f0609db340899
simon-perschel@t-online.de <bywguc-1mabtY-wigwaz>
2020-03-03 10:29:40 +0100

    fix: spotbugs error

--- src/test/java/mops/hhu/de/rheinjug1/praxis/PraxisApplicationTests.java
+++ src/test/java/mops/hhu/de/rheinjug1/praxis/PraxisApplicationTests.java
@@ -3,6 +3,7 @@ package mops.hhu.de.rheinjug1.praxis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SuppressWarnings("PMD")
 @SpringBootTest
 class PraxisApplicationTests {
 
