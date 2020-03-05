2794b9323f68f02318b5c7b8f323497f672b87b1
unknown <henning.mitzinger@t-online.de>
2020-03-05 10:13:50 +0100

    feat: keycloak linked

---
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/auth/KeycloakConfig.java
@@ -0,0 +1,18 @@
+package mops.hhu.de.rheinjug1.praxis.auth;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * WORKAROUND for https://issues.redhat.com/browse/KEYCLOAK-11282
+ * Bean should move into {@link SecurityConfig} once Bug has been resolved
+ */
+
+@Configuration
+public class KeycloakConfig {
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
