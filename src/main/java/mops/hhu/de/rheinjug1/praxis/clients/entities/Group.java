041f4ef61570cf6112a6a67e899d2594fe3bbf6f
lin <lin@sipgate.de>
2020-03-05 12:09:47 +0100

    chore: entities for meetup API response

---
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/clients/entities/Group.java
@@ -0,0 +1,17 @@
+package mops.hhu.de.rheinjug1.praxis.clients.entities;
+
+public class Group {
+  private int created;
+  private String name;
+  private int id;
+  private String join_mode;
+  private double lat;
+  private double lon;
+  private String urlname;
+  private String who;
+  private String localized_location;
+  private String state;
+  private String country;
+  private String region;
+  private String timezone;
+}
---
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/clients/entities/Venue.java
@@ -0,0 +1,13 @@
+package mops.hhu.de.rheinjug1.praxis.clients.entities;
+
+public class Venue {
+  private int id;
+  private String name;
+  private double lat;
+  private double lon;
+  private boolean repinned;
+  private String address_1;
+  private String city;
+  private String country;
+  private String localized_country_name;
+}
