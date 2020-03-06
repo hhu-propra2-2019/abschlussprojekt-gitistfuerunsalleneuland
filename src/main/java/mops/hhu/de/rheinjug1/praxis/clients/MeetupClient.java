4116a24939b23e3723fa46d08dfad798f89dc449
simon-perschel@t-online.de <bywguc-1mabtY-wigwaz>
2020-03-06 16:43:03 +0100

    fix: some fixes and service class created

--- src/main/java/mops/hhu/de/rheinjug1/praxis/clients/MeetupClient.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/clients/MeetupClient.java
@@ -1,8 +1,12 @@
 package mops.hhu.de.rheinjug1.praxis.clients;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import mops.hhu.de.rheinjug1.praxis.clients.dto.EventResponseDTO;
+import mops.hhu.de.rheinjug1.praxis.database.entities.Event;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -13,13 +17,13 @@ public class MeetupClient {
 
   @Value("${meetup.api.url}")
   private String meetupApiUrl;
+  private final RestTemplate restTemplate = new RestTemplate();
 
-  public List<EventResponseDTO> getUpcomingEvents() {
-    final RestTemplate restTemplate = new RestTemplate();
+
+  public List<Event> getUpcomingEvents() {
     final ResponseEntity<EventResponseDTO[]> response =
         restTemplate.getForEntity(
-            meetupApiUrl + "/events?status=upcoming", EventResponseDTO[].class);
-
-    return Arrays.asList(response.getBody());
+            meetupApiUrl + "/events?status=upcoming,past", EventResponseDTO[].class);
+    return Arrays.stream(response.getBody()).map(eventDto->eventDto.toEvent()).collect(Collectors.toList());
   }
 }
