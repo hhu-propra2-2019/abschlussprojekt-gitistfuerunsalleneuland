9cbfb856d8ebeb15fa5ab079a3d34fb1807b5fa6
unknown <henning.mitzinger@t-online.de>
2020-03-06 08:45:22 +0100

    refactor: PMD approved

--- src/main/java/mops/hhu/de/rheinjug1/praxis/controller/RheinjugController.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/controller/RheinjugController.java
@@ -10,54 +10,53 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import javax.annotation.security.RolesAllowed;
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RheinjugController {
 
-    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-    private final Counter authenticatedAccess;
-    private final Counter publicAccess;
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField", "PMD.UnusedImports"})
+  private final Counter authenticatedAccess;
 
-    public RheinjugController(MeterRegistry registry) {
-        authenticatedAccess = registry.counter("access.authenticated");
-        publicAccess = registry.counter("access.public");
-    }
+  private final Counter publicAccess;
 
-    private Account createAccountFromPrincipal(KeycloakAuthenticationToken token) {
-        KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
-        return new Account(
-                principal.getName(),
-                principal.getKeycloakSecurityContext().getIdToken().getEmail(),
-                null,
-                token.getAccount().getRoles());
-    }
+  public RheinjugController(final MeterRegistry registry) {
+    authenticatedAccess = registry.counter("access.authenticated");
+    publicAccess = registry.counter("access.public");
+  }
 
-    @GetMapping("/")
-    public String rheinjug(KeycloakAuthenticationToken token, Model model) {
-        if (token != null) {
-            model.addAttribute("account", createAccountFromPrincipal(token));
-        }
-        publicAccess.increment();
-        return "rheinjug";
-    }
+  private Account createAccountFromPrincipal(final KeycloakAuthenticationToken token) {
+    final KeycloakPrincipal principal = (KeycloakPrincipal) token.getPrincipal();
+    return new Account(
+        principal.getName(),
+        principal.getKeycloakSecurityContext().getIdToken().getEmail(),
+        null,
+        token.getAccount().getRoles());
+  }
 
-    @GetMapping("/logout")
-    public String logout(HttpServletRequest request) throws Exception {
-        request.logout();
-        return "redirect:/";
+  @GetMapping("/")
+  public String rheinjug(final KeycloakAuthenticationToken token, final Model model) {
+    if (token != null) {
+      model.addAttribute("account", createAccountFromPrincipal(token));
     }
-    
-	@GetMapping("/statistics")
-	@Secured("ROLE_orga")
-	public String statistics(KeycloakAuthenticationToken token, Model model) {
-		model.addAttribute("account", createAccountFromPrincipal(token));
-        return "statistics";
-	}
-	
-	@GetMapping("/talk")
-	public String talk() {
-		return "talk";
-	}
+    publicAccess.increment();
+    return "rheinjug";
+  }
+
+  @GetMapping("/logout")
+  public String logout(final HttpServletRequest request) throws ServletException {
+    request.logout();
+    return "redirect:/";
+  }
+
+  @GetMapping("/statistics")
+  @Secured("ROLE_orga")
+  public String statistics(final KeycloakAuthenticationToken token, final Model model) {
+    model.addAttribute("account", createAccountFromPrincipal(token));
+    return "statistics";
+  }
+
+  @GetMapping("/talk")
+  public String talk() {
+    return "talk";
+  }
 }
