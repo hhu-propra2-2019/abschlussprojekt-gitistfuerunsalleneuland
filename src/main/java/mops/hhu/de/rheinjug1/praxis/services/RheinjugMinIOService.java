34e84d82bf69fb7a208b6046d1d8ce34aafef82e
alihoe <alica.hoevelmeyer@live.de>
2020-03-06 12:27:50 +0100

    feat starting to create minio get-mapping

--- src/main/java/mops/hhu/de/rheinjug1/praxis/services/RheinjugMinIOService.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/services/RheinjugMinIOService.java
@@ -5,12 +5,19 @@ import java.io.IOException;
 import com.jlefebure.spring.boot.minio.MinioException;
 import com.jlefebure.spring.boot.minio.MinioService;
 import java.io.InputStream;
+import java.net.URLConnection;
 import java.nio.file.Path;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.http.entity.ContentType;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 
+@Service
 public class RheinjugMinIOService {
 	
 	@Autowired
@@ -32,5 +39,17 @@ public class RheinjugMinIOService {
 			e.printStackTrace();
 		}
 	}
+	
+	public void getObject(String object, HttpServletResponse response) throws MinioException, IOException {
+        InputStream inputStream = minioService.get(Path.of(object));
+
+        // Set the content type and attachment header.
+        response.addHeader("Content-disposition", "attachment;filename=" + object);
+        response.setContentType(URLConnection.guessContentTypeFromName(object));
+
+        // Copy the stream to the response's output stream.
+        IOUtils.copy(inputStream, response.getOutputStream());
+        response.flushBuffer();
+    }
 
 }
