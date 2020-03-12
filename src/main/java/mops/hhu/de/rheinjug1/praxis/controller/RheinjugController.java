7d5b5e36a62ce75a498dc193997fae7c2de6c390
lin <lin@sipgate.de>
2020-03-12 11:49:18 +0100

    fix: remove class files, fix pmd warnings

--- src/main/java/mops/hhu/de/rheinjug1/praxis/controller/RheinjugController.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/controller/RheinjugController.java
@@ -1,34 +1,22 @@
 package mops.hhu.de.rheinjug1.praxis.controller;
 
-import java.io.IOException;
-import javax.servlet.http.HttpServletResponse;
+import static mops.hhu.de.rheinjug1.praxis.thymeleaf.ThymeleafAttributesHelper.ACCOUNT_ATTRIBUTE;
+
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import java.io.File;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import mops.hhu.de.rheinjug1.praxis.models.Account;
+import mops.hhu.de.rheinjug1.praxis.models.Summary;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 
 @Controller
-@SuppressWarnings({
-  "PMD.UnusedPrivateField",
-  "PMD.SingularField",
-  "PMD.UnusedImports",
-  "PMD.AvoidDuplicateLiterals"
-})
+@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
 public class RheinjugController {
 
   private final Counter authenticatedAccess;
@@ -52,48 +40,41 @@ public class RheinjugController {
   @GetMapping("/")
   public String uebersicht(final KeycloakAuthenticationToken token, final Model model) {
     if (token != null) {
-      model.addAttribute("account", createAccountFromPrincipal(token));}
+      model.addAttribute(ACCOUNT_ATTRIBUTE, createAccountFromPrincipal(token));
+    }
     publicAccess.increment();
     return "uebersicht";
   }
-  
+
   @GetMapping("/talk")
   @Secured("ROLE_orga")
   public String statistics(final KeycloakAuthenticationToken token, final Model model) {
-	  if (token != null) model.addAttribute("account", createAccountFromPrincipal(token));
-	  model.addAttribute("summaryForm", new Summary()); 
-      return "talk";
+    if (token != null) {
+      model.addAttribute(ACCOUNT_ATTRIBUTE, createAccountFromPrincipal(token));
+    }
+    model.addAttribute("summaryForm", new Summary());
+    return "talk";
   }
-  
-  
-  @PostMapping("/talk")
-  @Secured({"ROLE_student", "ROLE_orga"})
-	public String handleFileUpload(@RequestParam("summary") MultipartFile file) {
-	  return "redirect:/talk/";
 
-  }
-  
-  @GetMapping("/logout") 
+  @GetMapping("/logout")
   public String logout(final HttpServletRequest request) throws ServletException {
     request.logout();
     return "redirect:/";
   }
 
-  
-
   @GetMapping("/profil")
   public String profil(final KeycloakAuthenticationToken token, final Model model) {
-	  if (token != null) model.addAttribute("account", createAccountFromPrincipal(token));
-	   return "profil";
+    if (token != null) {
+      model.addAttribute(ACCOUNT_ATTRIBUTE, createAccountFromPrincipal(token));
+    }
+    return "profil";
   }
 
   @GetMapping("/statistics")
   public String talk(final KeycloakAuthenticationToken token, final Model model) {
-    if (token != null) model.addAttribute("account", createAccountFromPrincipal(token));
+    if (token != null) {
+      model.addAttribute(ACCOUNT_ATTRIBUTE, createAccountFromPrincipal(token));
+    }
     return "statistics";
   }
-
-  
-
-		
 }
