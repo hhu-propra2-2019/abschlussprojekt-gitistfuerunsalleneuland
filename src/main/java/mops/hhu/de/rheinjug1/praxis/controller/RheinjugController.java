2dcac61ad1e3826c6d981388a9408df8ab01bef7
simon-perschel@t-online.de <bywguc-1mabtY-wigwaz>
2020-03-12 11:36:25 +0100

    feat: added thymeleaf attribute in controller

--- src/main/java/mops/hhu/de/rheinjug1/praxis/controller/RheinjugController.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/controller/RheinjugController.java
@@ -1,15 +1,16 @@
 package mops.hhu.de.rheinjug1.praxis.controller;
 
-import java.io.IOException;
-import javax.servlet.http.HttpServletResponse;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 
-import java.io.File;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+
+import mops.hhu.de.rheinjug1.praxis.database.entities.Event;
+import mops.hhu.de.rheinjug1.praxis.services.MeetupService;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,10 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @SuppressWarnings({
@@ -31,13 +30,14 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 })
 public class RheinjugController {
 
+  @Autowired private final MeetupService meetupService;
   private final Counter authenticatedAccess;
-
   private final Counter publicAccess;
 
-  public RheinjugController(final MeterRegistry registry) {
+  public RheinjugController(final MeterRegistry registry, MeetupService meetupService) {
     authenticatedAccess = registry.counter("access.authenticated");
     publicAccess = registry.counter("access.public");
+    this.meetupService = meetupService;
   }
 
   private Account createAccountFromPrincipal(final KeycloakAuthenticationToken token) {
@@ -53,6 +53,8 @@ public class RheinjugController {
   public String uebersicht(final KeycloakAuthenticationToken token, final Model model) {
     if (token != null) {
       model.addAttribute("account", createAccountFromPrincipal(token));}
+    List<Event> upcomingEvents = meetupService.getUpcomingEvents();
+    model.addAttribute("events",upcomingEvents);
     publicAccess.increment();
     return "uebersicht";
   }
