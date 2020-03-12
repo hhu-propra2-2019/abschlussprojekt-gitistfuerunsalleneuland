db8aba1c1b901a1a34ae3d0d6e98095b5f3fa4d2
lin <lin@sipgate.de>
2020-03-12 10:53:09 +0100

    test: add receiptService test

--- src/main/java/mops/hhu/de/rheinjug1/praxis/models/Receipt.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/models/Receipt.java
@@ -1,11 +1,13 @@
 package mops.hhu.de.rheinjug1.praxis.models;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import mops.hhu.de.rheinjug1.praxis.enums.MeetupType;
 
 @AllArgsConstructor
 @ToString
+@EqualsAndHashCode
 public class Receipt {
 
   private final String name;
