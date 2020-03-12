f73f310d4c450e32aa54a395e36e86a829b1d929
lin <lin@sipgate.de>
2020-03-12 08:49:53 +0100

    refactor: reverse exception handling

--- src/main/java/mops/hhu/de/rheinjug1/praxis/services/ReceiptService.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/services/ReceiptService.java
@@ -1,6 +1,8 @@
 package mops.hhu.de.rheinjug1.praxis.services;
 
-import io.vavr.control.Option;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 import mops.hhu.de.rheinjug1.praxis.entities.AcceptedSubmission;
 import mops.hhu.de.rheinjug1.praxis.entities.ReceiptSignature;
 import mops.hhu.de.rheinjug1.praxis.enums.MeetupType;
@@ -12,33 +14,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReceiptService {
 
-    @Autowired
-    private EncryptionService encryptionService;
+  @Autowired private EncryptionService encryptionService;
 
-    @Autowired
-    private MeetupService meetupService;
+  @Autowired private MeetupService meetupService;
 
-    @Autowired
-    private ReceiptSignatureRepository receiptSignatureRepository;
+  @Autowired private ReceiptSignatureRepository receiptSignatureRepository;
 
-    public Option<Receipt> createReceiptAndSaveSignatureInDatabase(final AcceptedSubmission submission, final String name) {
-        final long meetupId = submission.getMeetupId();
-        final MeetupType meetupType = meetupService.getType(meetupId);
+  public Receipt createReceiptAndSaveSignatureInDatabase(
+      final AcceptedSubmission submission, final String name)
+      throws UnrecoverableEntryException, NoSuchAlgorithmException, IOException,
+          CertificateException, KeyStoreException, SignatureException, InvalidKeyException {
+    final long meetupId = submission.getMeetupId();
+    final MeetupType meetupType = meetupService.getType(meetupId);
 
-        final String meetUpTitle = meetupService.getTitle(meetupId);
+    final String meetUpTitle = meetupService.getTitle(meetupId);
 
-        final Option<String> signatureOption = encryptionService.sign(meetupType, submission.getKeycloakId(), meetupId);
+    final String signature =
+        encryptionService.sign(meetupType, submission.getKeycloakId(), meetupId);
 
-        if(signatureOption.isEmpty()) {
-            return Option.none();
-        }
-
-        final String signature = signatureOption.get();
-
-        final ReceiptSignature receiptSignature = new ReceiptSignature(signature);
-        receiptSignatureRepository.save(receiptSignature);
-        return Option.of(new Receipt(name, meetupId, meetUpTitle, meetupType, signature));
-    }
-
-    ;
+    final ReceiptSignature receiptSignature = new ReceiptSignature(signature);
+    receiptSignatureRepository.save(receiptSignature);
+    return new Receipt(name, meetupId, meetUpTitle, meetupType, signature);
+  };
 }
