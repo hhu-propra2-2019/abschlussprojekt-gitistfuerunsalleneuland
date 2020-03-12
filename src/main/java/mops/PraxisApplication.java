070bd43c901a38fa2193e8f8a6c3b23ee253f30c
get4flo <florianhoffmann97@yahoo.com>
2020-03-12 11:39:40 +0100

    fix: forgot to add PMD/Spotbugs changes

--- src/main/java/mops/PraxisApplication.java
+++ src/main/java/mops/PraxisApplication.java
@@ -1,14 +1,5 @@
 package mops;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.file.Path;
-
-import org.apache.http.entity.ContentType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
--- src/main/java/mops/hhu/de/rheinjug1/praxis/controller/RheinjugController.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/controller/RheinjugController.java
@@ -1,11 +1,7 @@
 package mops.hhu.de.rheinjug1.praxis.controller;
 
-import java.io.IOException;
-import javax.servlet.http.HttpServletResponse;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-
-import java.io.File;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import org.keycloak.KeycloakPrincipal;
@@ -17,10 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @SuppressWarnings({
@@ -52,48 +44,47 @@ public class RheinjugController {
   @GetMapping("/")
   public String uebersicht(final KeycloakAuthenticationToken token, final Model model) {
     if (token != null) {
-      model.addAttribute("account", createAccountFromPrincipal(token));}
+      model.addAttribute("account", createAccountFromPrincipal(token));
+    }
     publicAccess.increment();
     return "uebersicht";
   }
-  
+
   @GetMapping("/talk")
   @Secured("ROLE_orga")
   public String statistics(final KeycloakAuthenticationToken token, final Model model) {
-	  if (token != null) model.addAttribute("account", createAccountFromPrincipal(token));
-	  model.addAttribute("summaryForm", new Summary()); 
-      return "talk";
+    if (token != null) {
+      model.addAttribute("account", createAccountFromPrincipal(token));
+    }
+    model.addAttribute("summaryForm", new Summary());
+    return "talk";
   }
-  
-  
+
   @PostMapping("/talk")
   @Secured({"ROLE_student", "ROLE_orga"})
-	public String handleFileUpload(@RequestParam("summary") MultipartFile file) {
-	  return "redirect:/talk/";
-
+  public String handleFileUpload(@RequestParam("summary") final MultipartFile file) {
+    return "redirect:/talk/";
   }
-  
-  @GetMapping("/logout") 
+
+  @GetMapping("/logout")
   public String logout(final HttpServletRequest request) throws ServletException {
     request.logout();
     return "redirect:/";
   }
 
-  
-
   @GetMapping("/profil")
   public String profil(final KeycloakAuthenticationToken token, final Model model) {
-	  if (token != null) model.addAttribute("account", createAccountFromPrincipal(token));
-	   return "profil";
+    if (token != null) {
+      model.addAttribute("account", createAccountFromPrincipal(token));
+    }
+    return "profil";
   }
 
   @GetMapping("/statistics")
   public String talk(final KeycloakAuthenticationToken token, final Model model) {
-    if (token != null) model.addAttribute("account", createAccountFromPrincipal(token));
+    if (token != null) {
+      model.addAttribute("account", createAccountFromPrincipal(token));
+    }
     return "statistics";
   }
-
-  
-
-		
 }
