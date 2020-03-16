e4d3d5a330dcd1a90e1e9cd757876876f9905fdc
lin <lin@sipgate.de>
2020-03-16 10:36:23 +0100

    feat: new route for creating receipt by submission id

---
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/services/SubmissionService.java
@@ -0,0 +1,37 @@
+package mops.hhu.de.rheinjug1.praxis.services;
+
+import java.util.Objects;
+import java.util.Optional;
+import mops.hhu.de.rheinjug1.praxis.database.entities.AcceptedSubmission;
+import mops.hhu.de.rheinjug1.praxis.database.repositories.AcceptedSubmissionRepository;
+import mops.hhu.de.rheinjug1.praxis.models.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubmissionService {
+
+  private final AcceptedSubmissionRepository acceptedSubmissionRepository;
+
+  @Autowired
+  public SubmissionService(final AcceptedSubmissionRepository acceptedSubmissionRepository) {
+    this.acceptedSubmissionRepository = acceptedSubmissionRepository;
+  }
+
+  public Optional<AcceptedSubmission> validateSubmission(
+      final Long submissionId, final Account account) {
+    final Optional<AcceptedSubmission> acceptedSubmissionOptional =
+        acceptedSubmissionRepository.findById(submissionId);
+    if (acceptedSubmissionOptional.isEmpty()) {
+      return acceptedSubmissionOptional;
+    }
+
+    final AcceptedSubmission acceptedSubmission = acceptedSubmissionOptional.get();
+    if (Objects.equals(acceptedSubmission.getName(), account.getName())
+        && Objects.equals(acceptedSubmission.getEmail(), account.getEmail())) {
+      return acceptedSubmissionOptional;
+    }
+
+    return Optional.empty();
+  }
+}
