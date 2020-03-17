e8555705a6790b46c9219ccb27e971b029f79422
alihoe <alica.hoevelmeyer@live.de>
2020-03-17 17:36:29 +0100

    fix/PMD Upload- and Download-Service

--- src/main/java/mops/hhu/de/rheinjug1/praxis/services/DownloadService.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/services/DownloadService.java
@@ -1,12 +1,5 @@
 package mops.hhu.de.rheinjug1.praxis.services;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
-import org.springframework.stereotype.Service;
-import org.xmlpull.v1.XmlPullParserException;
-
 import io.minio.MinioClient;
 import io.minio.errors.ErrorResponseException;
 import io.minio.errors.InternalException;
@@ -14,15 +7,22 @@ import io.minio.errors.InvalidExpiresRangeException;
 import io.minio.errors.InvalidResponseException;
 import io.minio.errors.MinioException;
 import io.minio.errors.NoResponseException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import org.springframework.stereotype.Service;
+import org.xmlpull.v1.XmlPullParserException;
 
 @Service
 public class DownloadService {
 
   public String getURLforObjectDownload(final String filename)
-      throws IOException, NoSuchAlgorithmException, InvalidKeyException, XmlPullParserException, MinioException, NoResponseException, ErrorResponseException, InternalException, InvalidExpiresRangeException, InvalidResponseException {
-	  String url = "";
-      final MinioClient minioClient = new MinioClient("http://localhost:9000/", "minio", "minio123");
-      url = minioClient.presignedGetObject("rheinjug", filename);
-      return url;
+      throws IOException, NoSuchAlgorithmException, InvalidKeyException, XmlPullParserException,
+          MinioException, NoResponseException, ErrorResponseException, InternalException,
+          InvalidExpiresRangeException, InvalidResponseException {
+    String url = "";
+    final MinioClient minioClient = new MinioClient("http://localhost:9000/", "minio", "minio123");
+    url = minioClient.presignedGetObject("rheinjug", filename);
+    return url;
   }
 }
