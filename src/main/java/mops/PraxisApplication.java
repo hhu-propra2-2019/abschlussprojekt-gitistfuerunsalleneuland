b5be3aa5df788e33e35c10a12dd2322168996e48
alihoe <alica.hoevelmeyer@live.de>
2020-03-17 10:31:26 +0100

    feat/Download and Upload Service finished

--- src/main/java/mops/PraxisApplication.java
+++ src/main/java/mops/PraxisApplication.java
@@ -1,11 +1,7 @@
 package mops;
 
-import mops.hhu.de.rheinjug1.praxis.services.DownloadService;
-import mops.hhu.de.rheinjug1.praxis.services.UploadService;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SuppressWarnings("PMD")
@@ -15,12 +11,5 @@ public class PraxisApplication {
 
   public static void main(final String[] args) {
     SpringApplication.run(PraxisApplication.class, args);
-  }
-
-  @Bean
-  ApplicationRunner init(DownloadService downloadService) {
-    return args -> {
-      downloadService.getURLforObjectDownload();
-    };
   }
 }
--- src/main/java/mops/hhu/de/rheinjug1/praxis/services/UploadService.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/services/UploadService.java
@@ -1,28 +1,23 @@
 package mops.hhu.de.rheinjug1.praxis.services;
 
-import org.springframework.stereotype.Service;
-
 import io.minio.MinioClient;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UploadService {
 
-    private MinioClient minioClient;
+  private MinioClient minioClient;
 
-    public UploadService() throws Exception {
+  public UploadService() throws Exception {
 
-        minioClient = new MinioClient("http://localhost:9000/",
-                "minio",
-                "minio123");
+    minioClient = new MinioClient("http://localhost:9000/", "minio", "minio123");
 
-        if (!minioClient.bucketExists("rheinjug")) {
-            minioClient.makeBucket("rheinjug");
-        }
+    if (!minioClient.bucketExists("rheinjug")) {
+      minioClient.makeBucket("rheinjug");
     }
+  }
 
-    public void uploadFile(final String name, final String localFilePath)
-            throws Exception {
-        minioClient.putObject("rheinjug", name, localFilePath, null,
-                null, null, null);
-    }
+  public void uploadFile(final String name, final String localFilePath) throws Exception {
+    minioClient.putObject("rheinjug", name, localFilePath, null, null, null, null);
+  }
 }
