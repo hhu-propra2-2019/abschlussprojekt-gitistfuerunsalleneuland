de20692ddcda7d6b43176633533b8cd62e50c795
alihoe <alica.hoevelmeyer@live.de>
2020-03-17 10:31:26 +0100

    feat/Download and Upload Service finished

--- src/main/java/mops/hhu/de/rheinjug1/praxis/services/DownloadService.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/services/DownloadService.java
@@ -1,31 +1,26 @@
 package mops.hhu.de.rheinjug1.praxis.services;
 
+import io.minio.MinioClient;
+import io.minio.errors.MinioException;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-
 import org.springframework.stereotype.Service;
 import org.xmlpull.v1.XmlPullParserException;
 
-import io.minio.MinioClient;
-import io.minio.errors.MinioException;
-
 @Service
 public class DownloadService {
-	
-	public String getURLforObjectDownload()
-		      throws IOException, NoSuchAlgorithmException, InvalidKeyException, XmlPullParserException {
-			String url = "";
-		    try {
-		   
-		      MinioClient minioClient =
-		          new MinioClient("http://localhost:9000/", "minio","minio123");
-		     url = minioClient.presignedGetObject("rheinjug", "testfile.adoc");
-		      System.out.println(url);
-		    } catch (MinioException e) {
-		      System.out.println("Error occurred: " + e);
-		    }
-		    return url;
-		  }
 
+  public String getURLforObjectDownload(String filename)
+      throws IOException, NoSuchAlgorithmException, InvalidKeyException, XmlPullParserException {
+    String url = "";
+    try {
+
+      MinioClient minioClient = new MinioClient("http://localhost:9000/", "minio", "minio123");
+      url = minioClient.presignedGetObject("rheinjug", filename);
+    } catch (MinioException e) {
+      System.out.println("Error occurred: " + e);
+    }
+    return url;
+  }
 }
