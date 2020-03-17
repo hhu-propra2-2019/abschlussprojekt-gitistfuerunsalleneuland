f5158f9885ac12f7925d735591ff91b1dfea018e
alihoe <alica.hoevelmeyer@live.de>
2020-03-17 17:19:54 +0100

    fix/PMD Upload- and Download-Service

--- src/main/java/mops/hhu/de/rheinjug1/praxis/services/UploadService.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/services/UploadService.java
@@ -1,14 +1,22 @@
 package mops.hhu.de.rheinjug1.praxis.services;
 
-import io.minio.MinioClient;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.stereotype.Service;
+import org.xmlpull.v1.XmlPullParserException;
+
+import io.minio.MinioClient;
+import io.minio.errors.InvalidBucketNameException;
+import io.minio.errors.MinioException;
 
 @Service
 public class UploadService {
 
-  private MinioClient minioClient;
+  private final MinioClient minioClient;
 
-  public UploadService() throws Exception {
+  public UploadService() throws MinioException, InvalidKeyException, NoSuchAlgorithmException, IOException, XmlPullParserException {
 
     minioClient = new MinioClient("http://localhost:9000/", "minio", "minio123");
 
@@ -17,7 +25,7 @@ public class UploadService {
     }
   }
 
-  public void uploadFile(final String name, final String localFilePath) throws Exception {
+  public void uploadFile(final String name, final String localFilePath) throws InvalidKeyException, InvalidBucketNameException, NoSuchAlgorithmException, MinioException, IOException, XmlPullParserException {
     minioClient.putObject("rheinjug", name, localFilePath, null, null, null, null);
   }
 }
---
+++ src/test/java/mops/hhu/de/rheinjug1/praxis/services/UploadServiceTest.java
@@ -0,0 +1,38 @@
+package mops.hhu.de.rheinjug1.praxis.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.minio.MinioClient;
+import io.minio.errors.MinioException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.xmlpull.v1.XmlPullParserException;
+
+@SpringBootTest
+public class UploadServiceTest {
+
+  static MinioClient minioClient;
+
+  @BeforeAll
+  static void createMinioClient() throws MinioException {
+    minioClient = new MinioClient("http://localhost:9000/", "minio", "minio123");
+  }
+
+  @Test
+  void bucketExistenceTest()
+      throws MinioException, InvalidKeyException, NoSuchAlgorithmException, IOException,
+          XmlPullParserException {
+    assertThat(minioClient.bucketExists("rheinjug")).isTrue();
+  }
+
+  @Test
+  void uploadTestfile() throws Exception {
+    minioClient = new MinioClient("http://localhost:9000/", "minio", "minio123");
+    minioClient.putObject(   "rheinjug", "testfile", "src/test/resources/test.txt", null, null, null, null);
+    minioClient.statObject("rheinjug", "testfile");
+  }
+}
