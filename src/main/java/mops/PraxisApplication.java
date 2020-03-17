d5435e06764aa06a780db4af32678c8b1e47a409
hjizerena <49520474+hjizerena@users.noreply.github.com>
2020-03-17 11:41:16 +0100

    remove unnecessary imports and fields

--- src/main/java/mops/PraxisApplication.java
+++ src/main/java/mops/PraxisApplication.java
@@ -1,8 +1,5 @@
 package mops;
 
-import mops.hhu.de.rheinjug1.praxis.database.entities.Submission;
-import mops.hhu.de.rheinjug1.praxis.database.repositories.SubmissionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class PraxisApplication {
-
-  @Autowired SubmissionRepository submissionRepository;
 
   public static void main(final String[] args) {
     SpringApplication.run(PraxisApplication.class, args);
