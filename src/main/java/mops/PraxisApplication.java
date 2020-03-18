f7b94aa3354bd7b455378f3c4ff9b3a912d15b09
Alihoe <49514273+Alihoe@users.noreply.github.com>
2020-03-18 09:45:40 +0100

    Application-Runner entfernt

--- src/main/java/mops/PraxisApplication.java
+++ src/main/java/mops/PraxisApplication.java
@@ -1,12 +1,9 @@
 package mops;
 
-import mops.hhu.de.rheinjug1.praxis.database.entities.Submission;
 import mops.hhu.de.rheinjug1.praxis.database.repositories.SubmissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SuppressWarnings("PMD")
@@ -20,35 +17,4 @@ public class PraxisApplication {
     SpringApplication.run(PraxisApplication.class, args);
   }
 
-  @Bean
-  ApplicationRunner init() {
-    return args -> {
-      final Submission submission =
-          Submission.builder()
-              .meetupId(256264912L)
-              .email("hajoa100@hhu.de")
-              .name("hjizerena")
-              .minIoLink("")
-              .build();
-      final Submission acceptedSubmission =
-          Submission.builder()
-              .name("hjizerena")
-              .accepted(true)
-              .email("hajoa100@hhu.de")
-              .meetupId(247416797L)
-              .minIoLink("")
-              .build();
-      final Submission anotherSubmission =
-          Submission.builder()
-              .meetupId(256264912L)
-              .email("ottolin@outlook.de")
-              .name("otlin100")
-              .minIoLink("")
-              .build();
-      Thread.sleep(5000);
-      submissionRepository.save(submission);
-      submissionRepository.save(acceptedSubmission);
-      submissionRepository.save(anotherSubmission);
-    };
-  };
 }
