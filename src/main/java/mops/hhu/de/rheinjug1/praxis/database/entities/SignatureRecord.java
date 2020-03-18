96ed4686d85a1da23bb6c0b881e610bfe41dd6d2
hjizerena <hajoa100@hhu.de>
2020-03-18 10:27:24 +0100

    All args constructor for Signature Record

--- src/main/java/mops/hhu/de/rheinjug1/praxis/database/entities/SignatureRecord.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/database/entities/SignatureRecord.java
@@ -1,21 +1,15 @@
 package mops.hhu.de.rheinjug1.praxis.database.entities;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
 public class SignatureRecord {
+  
   @Id private String signature;
   private Long meetupId;
-
-  public SignatureRecord(final String signature, final Long meetupId) {
-    this.signature = signature;
-    this.meetupId = meetupId;
-  }
 }
