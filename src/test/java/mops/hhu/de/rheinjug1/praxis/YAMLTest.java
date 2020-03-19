c34b6264d109860e354dcbb47df4d4f1016d0251
hjizerena <hajoa100@hhu.de>
2020-03-19 11:39:19 +0100

    yaml read and write tests

--- src/test/java/mops/hhu/de/rheinjug1/praxis/YAMLTest.java
+++ src/test/java/mops/hhu/de/rheinjug1/praxis/YAMLTest.java
@@ -1,28 +1,54 @@
 package mops.hhu.de.rheinjug1.praxis;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import mops.hhu.de.rheinjug1.praxis.enums.MeetupType;
-import mops.hhu.de.rheinjug1.praxis.models.Receipt;
+import mops.hhu.de.rheinjug1.praxis.models.ReceiptDTO;
 import org.junit.jupiter.api.Test;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Writer;
-
 public class YAMLTest {
-    @Test
-    void writeYml() throws IOException {
-        final Yaml yaml = new Yaml();
-        final Writer fileWriter = new FileWriter("test.yml");
-        final Long meetupId = 12_345L;
-        final String name = "testName";
-        final String email = "testEmail";
-        final String meetupTitle = "testMeetupTitle";
-        final String signature = "testSignature";
-        final MeetupType meetupType = MeetupType.ENTWICKELBAR;
-        final Receipt receipt =
-                new Receipt(name, email, meetupId, meetupTitle, meetupType, signature);
-        yaml.dump(receipt, fileWriter);
-    }
+
+  private static final String TEST_FILE_CONTENT =
+      "!!mops.hhu.de.rheinjug1.praxis.models.ReceiptDTO {email: testEmail, meetupId: 12345,\n"
+          + "  meetupType: ENTWICKELBAR, name: testName, signature: testSignature, title: testMeetupTitle}\n";
+
+  @Test
+  void writeYml() throws IOException {
+    final Yaml yaml = new Yaml();
+    final String path = File.createTempFile("receipt", ".tmp").getAbsolutePath();
+    final Writer fileWriter = new FileWriter(path);
+
+    final long meetupId = 12_345L;
+    final String name = "testName";
+    final String email = "testEmail";
+    final String meetupTitle = "testMeetupTitle";
+    final String signature = "testSignature";
+    final MeetupType meetupType = MeetupType.ENTWICKELBAR;
+
+    final ReceiptDTO receipt =
+        new ReceiptDTO(name, email, meetupId, meetupTitle, meetupType, signature);
+    yaml.dump(receipt, fileWriter);
+
+    assertThat(Files.readString(Paths.get(path))).isEqualTo(TEST_FILE_CONTENT);
+  }
+
+  @Test
+  void readYml() throws IOException {
+    final Yaml yaml = new Yaml();
+    final String path = File.createTempFile("receipt", ".tmp").getAbsolutePath();
+    final Writer fileWriter = new FileWriter(path);
+    final BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+    bufferedWriter.write(TEST_FILE_CONTENT);
+    bufferedWriter.close();
+
+    final Reader fileReader = new FileReader(path);
+
+    final ReceiptDTO receiptDTO = (ReceiptDTO) yaml.load(fileReader);
+
+    assertThat(receiptDTO.getName()).isEqualTo("testName");
+  }
 }
