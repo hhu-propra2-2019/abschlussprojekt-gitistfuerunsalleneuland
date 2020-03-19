3f91be65a250962d5ae7d69a32b85cf5c9445004
hjizerena <hajoa100@hhu.de>
2020-03-19 23:00:42 +0100

    Create Test SendMockMail

--- src/test/java/mops/hhu/de/rheinjug1/praxis/services/ReceiptSendServiceTest.java
+++ src/test/java/mops/hhu/de/rheinjug1/praxis/services/ReceiptSendServiceTest.java
@@ -2,13 +2,19 @@ package mops.hhu.de.rheinjug1.praxis.services;
 
 import java.io.IOException;
 import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import mops.hhu.de.rheinjug1.praxis.enums.MeetupType;
 import mops.hhu.de.rheinjug1.praxis.models.Receipt;
 import mops.hhu.de.rheinjug1.praxis.services.receipt.ReceiptPrintService;
+import mops.hhu.de.rheinjug1.praxis.services.receipt.ReceiptReadService;
 import mops.hhu.de.rheinjug1.praxis.services.receipt.ReceiptSendService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class ReceiptSendServiceTest {
@@ -20,5 +26,20 @@ class ReceiptSendServiceTest {
     final Receipt receipt =
         new Receipt("TestName", "TestEmail", 1L, "Titel", MeetupType.ENTWICKELBAR, "OEUIc5654eut");
     receiptSendService.sendReceipt(receipt, "rheinjughhu@gmail.com");
+  }
+
+  @Test
+  void SendMockMail() throws IOException, MessagingException {
+    final Receipt receipt  =
+            new Receipt("TestName", "TestEmail", 1L, "Titel", MeetupType.ENTWICKELBAR, "OEUIc5654eut");
+    final JavaMailSender mailSender = mock(JavaMailSender.class);
+    final MimeMessage mimeMessage = mock(MimeMessage.class);
+    when(mailSender.createMimeMessage()).thenReturn(mimeMessage);
+    final MailService mailService = new MailService(mailSender);
+    final ReceiptSendService receiptSendService = new ReceiptSendService(mailService, new ReceiptPrintService());
+
+    receiptSendService.sendReceipt(receipt, "rheinjughhu@gmail.com");
+
+    verify(mailSender).send(mimeMessage);
   }
 }
