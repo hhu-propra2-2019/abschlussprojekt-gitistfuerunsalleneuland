35bf952942d410a4651a4c970d8b74131d8ce66b
hjizerena <hajoa100@hhu.de>
2020-03-23 13:47:36 +0100

    apply yaml.dumpAsMap

--- src/main/java/mops/hhu/de/rheinjug1/praxis/services/receipt/ReceiptPrintService.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/services/receipt/ReceiptPrintService.java
@@ -1,45 +1,29 @@
 package mops.hhu.de.rheinjug1.praxis.services.receipt;
 
-import static java.nio.charset.StandardCharsets.*;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import mops.hhu.de.rheinjug1.praxis.models.Receipt;
 import org.bouncycastle.util.encoders.Base64;
 import org.springframework.stereotype.Service;
-import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.nodes.Tag;
-import org.yaml.snakeyaml.representer.Representer;
 
 @Service
 public class ReceiptPrintService {
 
-  private static String getYamlString(final Receipt receipt) {
-    final Representer representer = new Representer();
-    representer.addClassTag(Receipt.class, Tag.MAP);
-
-    final DumperOptions options = new DumperOptions();
-    options.setIndent(2);
-    options.setPrettyFlow(true);
-    options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-
-    return new Yaml(representer, options).dump(receipt);
-  }
-
   public String printReceiptAndReturnPath(final Receipt receipt) throws IOException {
     final String path = File.createTempFile("receipt", ".tmp").getAbsolutePath();
 
-    try (FileWriter fileWriter = new FileWriter(path);
-        PrintWriter printWriter = new PrintWriter(fileWriter)) {
-
+    try (FileWriter fileWriter = new FileWriter(path)) {
       fileWriter.write(Base64.toBase64String(getYamlString(receipt).getBytes(UTF_8)));
-
-    } catch (IOException e) {
-      e.printStackTrace();
     }
     return path;
+  }
+
+  private static String getYamlString(final Receipt receipt) {
+    final Yaml yaml = new Yaml();
+    return yaml.dumpAsMap(receipt);
   }
 }
--- src/test/java/mops/hhu/de/rheinjug1/praxis/services/ReceiptPrintServiceTest.java
+++ src/test/java/mops/hhu/de/rheinjug1/praxis/services/ReceiptPrintServiceTest.java
@@ -1,6 +1,6 @@
 package mops.hhu.de.rheinjug1.praxis.services;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -30,7 +30,6 @@ class ReceiptPrintServiceTest {
     final String path = receiptPrintService.printReceiptAndReturnPath(receipt);
 
     final String expectedMailText = FileUtils.readStringFromFile("mail/testEmailAttachment.txt");
-
     final String actualMailText = new String(Base64.decode(Files.readString(Paths.get(path))));
 
     assertThat(actualMailText).isEqualTo(expectedMailText);
