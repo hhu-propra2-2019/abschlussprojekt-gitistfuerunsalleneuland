b72f30b1efb8bd8b086b2c725e55e2975c14daee
lin <lin@sipgate.de>
2020-03-23 12:48:44 +0100

    refactor: pretty yaml dump, base64 encoding

--- src/main/java/mops/hhu/de/rheinjug1/praxis/services/receipt/ReceiptPrintService.java
+++ src/main/java/mops/hhu/de/rheinjug1/praxis/services/receipt/ReceiptPrintService.java
@@ -1,23 +1,41 @@
 package mops.hhu.de.rheinjug1.praxis.services.receipt;
 
+import static java.nio.charset.StandardCharsets.*;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import mops.hhu.de.rheinjug1.praxis.models.Receipt;
+import org.bouncycastle.util.encoders.Base64;
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Representer;
 
 @Service
 public class ReceiptPrintService {
 
-  public String printReceipt(final Receipt receipt) throws IOException {
+  private static String getYamlString(final Receipt receipt) {
+    final Representer representer = new Representer();
+    representer.addClassTag(Receipt.class, Tag.MAP);
+
+    final DumperOptions options = new DumperOptions();
+    options.setIndent(2);
+    options.setPrettyFlow(true);
+    options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+
+    return new Yaml(representer, options).dump(receipt);
+  }
+
+  public String printReceiptAndReturnPath(final Receipt receipt) throws IOException {
     final String path = File.createTempFile("receipt", ".tmp").getAbsolutePath();
 
     try (FileWriter fileWriter = new FileWriter(path);
         PrintWriter printWriter = new PrintWriter(fileWriter)) {
-      final Yaml yaml = new Yaml();
-      yaml.dump(receipt, fileWriter);
+
+      fileWriter.write(Base64.toBase64String(getYamlString(receipt).getBytes(UTF_8)));
 
     } catch (IOException e) {
       e.printStackTrace();
