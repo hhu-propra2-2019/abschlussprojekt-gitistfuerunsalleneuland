adfe348ff17589c134e7d8ee55c2c64555d6695f
alihoe <alica.hoevelmeyer@live.de>
2020-03-25 12:59:36 +0100

    fix/remove unused import

--- src/test/java/mops/hhu/de/rheinjug1/praxis/services/MinIoUploadServiceTest.java
+++ src/test/java/mops/hhu/de/rheinjug1/praxis/services/MinIoUploadServiceTest.java
@@ -12,7 +12,6 @@ import java.security.NoSuchAlgorithmException;
 import mops.hhu.de.rheinjug1.praxis.services.minio.MinIoUploadService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
