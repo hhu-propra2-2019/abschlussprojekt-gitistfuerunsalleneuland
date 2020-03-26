6dd18b36dfa81bef8d191d275250290f2162117c
Prince1337 <prince.pieritz@gmail.com>
2020-03-26 12:53:09 +0100

    merge

--- CertificationApp/src/main/java/mops/hhu/de/rheinjug1/praxis/repositories/ReceiptRepository.java
+++ CertificationApp/src/main/java/mops/hhu/de/rheinjug1/praxis/repositories/ReceiptRepository.java
@@ -1,11 +1,12 @@
 package mops.hhu.de.rheinjug1.praxis.repositories;
 
-import java.util.List;
 import mops.hhu.de.rheinjug1.praxis.entities.ReceiptEntity;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReceiptRepository extends CrudRepository<ReceiptEntity, Long> {
@@ -13,12 +14,11 @@ public interface ReceiptRepository extends CrudRepository<ReceiptEntity, Long> {
   @Query("SELECT * FROM rheinjug1.receipt WHERE receipt.id=:id")
   ReceiptEntity findReceiptEntityById(@Param("id") Long id);
 
-<<<<<<< HEAD
   @Query("SELECT COUNT(signature) FROM receipts.receipt r WHERE r.meetup_type = :meetup_type;")
   int countSignatureByMeetupType(@Param("meetup_type") String meetupType);
-}
-=======
+
+
   @Query("SELECT * FROM receipt WHERE receipt.signature = signature")
   List<ReceiptEntity> findReceiptEntityBySignature(@Param("signature") String signature);
 }
->>>>>>> CertificationApp
+
